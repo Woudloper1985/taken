d29972d38c9fe95c194184b156e5c6ed470f2f54
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-02-24 11:11:27 +0100

    JDBC AbstractRepository

--- src/main/java/be/vdab/taken/AbstractRepository.java
+++ src/main/java/be/vdab/taken/AbstractRepository.java
@@ -4,17 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Main {
+abstract class AbstractRepository {
 
     private static final String URL = "jdbc:mysql://localhost/bieren";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
 
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
-            System.out.println("Connectie open");
-        } catch (SQLException ex) {
-            ex.printStackTrace(System.err);
-        }
+    protected Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
