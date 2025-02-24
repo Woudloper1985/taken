92f27bfe538a6c5cf9de4bbebfe1eb104f95a73e
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-02-24 10:22:58 +0100

    JDBC taken initial commit

--- src/main/java/be/vdab/taken/Main.java
+++ src/main/java/be/vdab/taken/Main.java
@@ -1,7 +1,20 @@
 package be.vdab.taken;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
+
+    private static final String URL = "jdbc:mysql://localhost/bieren";
+    private static final String USER = "cursist";
+    private static final String PASSWORD = "cursist";
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            System.out.println("Connectie open");
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.err);
+        }
     }
 }
