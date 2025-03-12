fa1d4900a4d798eea6dc56c14b823d83371d7d0f
NickVandenbroeck <nick.vandenbroeck@vdabcampus.be>
2025-03-12 13:25:59 +0100

    Bank Rekening

---
+++ src/main/java/be/vdab/taken/test/Rekening.java
@@ -0,0 +1,9 @@
+package be.vdab.taken.test;
+
+public class Rekening {
+    private final String rekeningnummer;
+
+    public Rekening(String rekeningnummer) {
+        this.rekeningnummer = rekeningnummer;
+    }
+}
---
+++ src/main/java/be/vdab/taken/test/RekeningRepository.java
@@ -0,0 +1,5 @@
+package be.vdab.taken.test;
+
+public class RekeningRepository extends AbstractRepository {
+
+}
